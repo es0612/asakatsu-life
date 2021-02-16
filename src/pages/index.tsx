434c57657a1caad229743473e7a2ad665c0fc52f
shinya <shinya@example.com>
2021-02-17 05:20:06 +0900

    add article list for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,56 @@
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import type { FC } from 'react';
 import React from 'react';
+import type { IndexPageQuery } from '../../types/graphql-types';
 import { Layout } from '../components/layout';
 
-const Page: FC = () => (
+interface PageProps {
+  data: IndexPageQuery;
+}
+
+const Page: FC<PageProps> = ({ data }) => (
   <Layout>
-    <h1>Home</h1>
-    <p>Hello, world!</p>
-    <Link to="/about">about</Link>
+    <div className="article-list">
+      {data.allMarkdownRemark.edges.map(edge => {
+        if (!edge.node.frontmatter?.slug) return null;
+
+        const slug = edge.node.frontmatter.slug;
+
+        return (
+          <>
+            <Link key={slug} className="article-list-item" to={slug}>
+              {edge.node.frontmatter.date && (
+                <p className="article-list-item-date">
+                  {edge.node.frontmatter.date}
+                </p>
+              )}
+              <h1>{edge.node.frontmatter.title ?? '(無題)'}</h1>
+              {edge.node.excerpt && (
+                <p className="article-list-item-excerpt">{edge.node.excerpt}</p>
+              )}
+            </Link>
+            <hr />
+          </>
+        );
+      })}
+    </div>
   </Layout>
 );
 export default Page;
+
+export const query = graphql`
+  query IndexPage {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            date(formatString: "YYYY/MM/DD", locale: "ja-JP")
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`;
