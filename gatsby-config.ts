339122163762456874fea40cd7b852b07f21c7b4
shinya <shinya@example.com>
2021-02-16 17:18:10 +0900

    use first graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,12 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
+  siteMetadata: {
+    title: 'Asakatsu Blog',
+    author: 'Shinya Ema',
+    description: 'new blog using GatsbyJS',
+    email: 'emashinya0612@gmail.com'
+  },
   plugins: ['gatsby-plugin-sass']
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,22 @@
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import type { FC } from 'react';
 import React from 'react';
 
 export const Header: FC = () => {
+  const data = useStaticQuery(graphql`
+    query MyQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
   return (
     <header>
       <h1>
-        <Link to="/">Asakatsu Blog</Link>
+        <Link to="/">{data.site.siteMetadata.title}</Link>
       </h1>
     </header>
   );
