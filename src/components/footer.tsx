df56075427270b26515da44e14b533ee09f0f231
shinya <shinya@example.com>
2021-02-16 19:05:47 +0900

    update footer for using metadata.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,31 @@
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import type { FC } from 'react';
 import React from 'react';
+import type { FooterComponentQuery } from '../../types/graphql-types';
 
-export const Footer: FC = () => (
-  <footer>
-    <p>&copy; Shinya Ema</p>
-    <ul>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <a href="mailto:emashinya0612@gmail.com">Contact</a>
-      </li>
-    </ul>
-  </footer>
-);
+export const Footer: FC = () => {
+  const data = useStaticQuery<FooterComponentQuery>(graphql`
+    query FooterComponent {
+      site {
+        siteMetadata {
+          author
+          email
+        }
+      }
+    }
+  `);
+
+  return (
+    <footer>
+      <p>&copy; {data.site?.siteMetadata?.author}</p>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <a href={`mailto:${data.site?.siteMetadata?.email}`}>Contact</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
