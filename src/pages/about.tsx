b7c5a960f3c145c47fe2cc89304453b3c1ad3d73
shinya <shinya@example.com>
2021-02-16 16:13:19 +0900

    create second page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,10 @@
 import type { FC } from 'react';
 import React from 'react';
-import { Link } from 'gatsby';
 
 const Page: FC = () => (
   <div>
-    <h1>Home</h1>
-    <p>Hello, world!</p>
-    <Link to="/about">about</Link>
+    <h1>About</h1>
+    <p>GatsbyJSでできたブログ</p>
   </div>
 );
 export default Page;
